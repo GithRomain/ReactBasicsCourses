3e6010fdd19bec7e78d0536e993a441ab151da67
Romain Pasquier <romain.pasquier@efrei.net>
2023-03-14 20:26:14 +0100

    edit

--- notesApp/src/components/Editor/Editor.jsx
+++ notesApp/src/components/Editor/Editor.jsx
@@ -4,30 +4,30 @@ import Showdown from "showdown"
 import './Editor.css';
 
 function Editor({ currentNote, updateNote }) {
-  const [selectedTab, setSelectedTab] = React.useState("write")
+    const [selectedTab, setSelectedTab] = React.useState("write")
 
-  const converter = new Showdown.Converter({
-      tables: true,
-      simplifiedAutoLink: true,
-      strikethrough: true,
-      tasklists: true,
-  })  
+    const converter = new Showdown.Converter({
+        tables: true,
+        simplifiedAutoLink: true,
+        strikethrough: true,
+        tasklists: true,
+    })
 
-  return (
-      <section className="pane editor">
-          <ReactMde
-              value={currentNote.body}
-              onChange={updateNote}
-              selectedTab={selectedTab}
-              onTabChange={setSelectedTab}
-              generateMarkdownPreview={(markdown) =>
-                  Promise.resolve(converter.makeHtml(markdown))
-              }
-              minEditorHeight={80}
-              heightUnits="vh"
-          />
-      </section>
-  )
+    return (
+        <section className="pane editor">
+            <ReactMde
+                value={currentNote.body}
+                onChange={updateNote}
+                selectedTab={selectedTab}
+                onTabChange={setSelectedTab}
+                generateMarkdownPreview={(markdown) =>
+                    Promise.resolve(converter.makeHtml(markdown))
+                }
+                minEditorHeight={80}
+                heightUnits="vh"
+            />
+        </section>
+    )
 }
 
 export default Editor;
